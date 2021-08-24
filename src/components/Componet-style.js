e7d89f73051c189a32b2e01bde7a4f07cec3a69f
ehsan2793 <ehsan2793@gmail.com>
2021-08-23 20:56:13 -0700

    few changes

--- src/components/Componet-style.js
+++ src/components/Componet-style.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
  export  const Button = styled.button`
-   padding: 6px 10px;
+   padding: 6px 20px;
+ 
    margin: 5px;
    border: none;
    border-radius: 3px;
    color: white;
+   border-radius: 12px;
+   width:10%;
 
-   ${props => (props.type === 'primary' ? `background: #2196f3; font-weight: bold;  font-size: 24px;` : null)}
+   ${props => (props.type === 'primary' ? `background: #2196f3; font-weight: bold;  font-size: 20px;` : null)}
    ${props => (props.type === 'success' ? `background: #4caf50;` : null)}
    ${props => (props.type === 'danger' ? `background: #f44336;` : null)}
    ${props => (props.type === 'warning' ? `background: #fdd835;` : null)}
